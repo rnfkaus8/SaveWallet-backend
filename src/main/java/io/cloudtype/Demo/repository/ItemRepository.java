d21e5e259c48833feaa3f6ea979560352fe0569a
rnfkaus8 <kth4350@gmail.com>
2023-07-29 20:22:38 +0900

    생성일 기준 해당 월의 데이터를 가져오는 쿼리 추가

--- src/main/java/io/cloudtype/Demo/repository/ItemRepository.java
+++ src/main/java/io/cloudtype/Demo/repository/ItemRepository.java
@@ -4,5 +4,10 @@ import io.cloudtype.Demo.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
