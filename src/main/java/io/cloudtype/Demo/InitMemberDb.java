fe49f3925542f47130d371917c7ba390a56bb01c
rnfkaus8 <kth4350@gmail.com>
2023-07-31 20:52:44 +0900

    초기 데이터 추가

---
+++ src/main/java/io/cloudtype/Demo/InitMemberDb.java
@@ -0,0 +1,33 @@
+package io.cloudtype.Demo;
+
+import io.cloudtype.Demo.domain.Member;
+import io.cloudtype.Demo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class InitMemberDb {
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.initDb();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final MemberRepository memberRepository;
+
+        public void initDb() {
+            memberRepository.save(Member.builder().createdAt(LocalDateTime.now()).id(1L).name("박채윤").build());
+            memberRepository.save(Member.builder().createdAt(LocalDateTime.now()).id(2L).name("박이영").build());
+        }
+    }
+}
