70456c218738fcd56272af40c838c76b8a7570df
rnfkaus8 <kth4350@gmail.com>
2023-08-02 19:51:02 +0900

    add findByName

--- src/main/java/io/cloudtype/Demo/repository/MemberRepository.java
+++ src/main/java/io/cloudtype/Demo/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package io.cloudtype.Demo.repository;
 import io.cloudtype.Demo.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
 }
