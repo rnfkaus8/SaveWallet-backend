cb2e3982619a83f1b309bc816bfdac3e802f91c3
rnfkaus8 <kth4350@gmail.com>
2023-08-03 19:35:26 +0900

    add findByMemberId

--- src/main/java/io/cloudtype/Demo/repository/CategoryRepository.java
+++ src/main/java/io/cloudtype/Demo/repository/CategoryRepository.java
@@ -2,6 +2,12 @@ package io.cloudtype.Demo.repository;
 
 import io.cloudtype.Demo.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("select c from Category c where c.member.id = :memberId")
+    List<Category> findByMemberId(@Param("memberId") Long id);
 }
