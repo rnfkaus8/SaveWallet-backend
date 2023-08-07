343e9bbed00ffb19c07b4e53f8373b52451defb9
rnfkaus8 <kth4350@gmail.com>
2023-08-07 19:50:11 +0900

    RequestParam 에서 날짜 받기
    ref : https://www.petrikainulainen.net/programming/spring-framework/spring-from-the-trenches-parsing-date-and-time-information-from-a-request-parameter/

--- src/main/java/io/cloudtype/Demo/controller/FindItemsRequest.java
+++ src/main/java/io/cloudtype/Demo/controller/FindItemsRequest.java
@@ -3,13 +3,18 @@ package io.cloudtype.Demo.controller;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Data
 @ToString
 public class FindItemsRequest {
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime startDate;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime endDate;
+
     private Long memberId;
 }
