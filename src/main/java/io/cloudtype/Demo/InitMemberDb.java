904d6836fece67fc74b7e1afceb2ea9345715851
rnfkaus8 <kth4350@gmail.com>
2023-08-31 21:05:03 +0900

    초기 카테고리 추가

--- src/main/java/io/cloudtype/Demo/InitMemberDb.java
+++ src/main/java/io/cloudtype/Demo/InitMemberDb.java
@@ -1,6 +1,8 @@
 package io.cloudtype.Demo;
 
+import io.cloudtype.Demo.domain.Category;
 import io.cloudtype.Demo.domain.Member;
+import io.cloudtype.Demo.repository.CategoryRepository;
 import io.cloudtype.Demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -24,10 +26,26 @@ public class InitMemberDb {
     @RequiredArgsConstructor
     static class InitService {
         private final MemberRepository memberRepository;
+        private final CategoryRepository categoryRepository;
 
         public void initDb() {
-            memberRepository.save(Member.builder().id(1L).name("박채윤").build());
-            memberRepository.save(Member.builder().id(2L).name("박이영").build());
+            Member 박채윤 = Member.builder().id(1L).name("박채윤").build();
+            Member 박이영 = Member.builder().id(2L).name("박이영").build();
+            memberRepository.save(박채윤);
+            memberRepository.save(박이영);
+            categoryRepository.save(Category.builder().member(박이영).name("식비").build());
+            categoryRepository.save(Category.builder().member(박이영).name("쇼핑").build());
+            categoryRepository.save(Category.builder().member(박이영).name("문화").build());
+            categoryRepository.save(Category.builder().member(박이영).name("취미/여가").build());
+            categoryRepository.save(Category.builder().member(박이영).name("교통").build());
+            categoryRepository.save(Category.builder().member(박이영).name("그 외").build());
+
+            categoryRepository.save(Category.builder().member(박채윤).name("식비").build());
+            categoryRepository.save(Category.builder().member(박채윤).name("쇼핑").build());
+            categoryRepository.save(Category.builder().member(박채윤).name("문화").build());
+            categoryRepository.save(Category.builder().member(박채윤).name("취미/여가").build());
+            categoryRepository.save(Category.builder().member(박채윤).name("교통").build());
+            categoryRepository.save(Category.builder().member(박채윤).name("그 외").build());
         }
     }
 }
