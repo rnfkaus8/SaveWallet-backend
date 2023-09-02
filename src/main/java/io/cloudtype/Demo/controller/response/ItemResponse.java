6c98b706c7fa2dd22ff0086846cb767a0c852ab7
rnfkaus8 <kth4350@gmail.com>
2023-09-02 16:05:00 +0900

    serialize response dto

--- src/main/java/io/cloudtype/Demo/controller/response/ItemResponse.java
+++ src/main/java/io/cloudtype/Demo/controller/response/ItemResponse.java
@@ -1,10 +1,14 @@
 package io.cloudtype.Demo.controller.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
+@ToString
+@Data
 public class ItemResponse {
 
     private Long id;
