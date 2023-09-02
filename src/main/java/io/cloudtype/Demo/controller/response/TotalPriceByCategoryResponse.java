2b6554e53b4a73645198739d032c49bf91cd7da3
rnfkaus8 <kth4350@gmail.com>
2023-09-02 16:05:20 +0900

    카테고리별 총 금액 response 객체 추가

---
+++ src/main/java/io/cloudtype/Demo/controller/response/TotalPriceByCategoryResponse.java
@@ -0,0 +1,13 @@
+package io.cloudtype.Demo.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Data
+public class TotalPriceByCategoryResponse {
+    private String categoryName;
+    private int totalPrice;
+}
