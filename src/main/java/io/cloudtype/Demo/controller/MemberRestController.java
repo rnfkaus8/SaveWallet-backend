6340807179390a9f6704f755293f5455726542ef
rnfkaus8 <kth4350@gmail.com>
2023-10-05 18:38:42 +0900

    ResponseEntity 적용

--- src/main/java/io/cloudtype/Demo/controller/MemberRestController.java
+++ src/main/java/io/cloudtype/Demo/controller/MemberRestController.java
@@ -5,6 +5,7 @@ import io.cloudtype.Demo.domain.Member;
 import io.cloudtype.Demo.repository.MemberRepository;
 import io.cloudtype.Demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,13 +15,13 @@ public class MemberRestController {
     private final MemberService memberService;
 
     @GetMapping("/member/{deviceId}")
-    public MemberResponse findByName(@PathVariable("deviceId") String deviceId) {
+    public ResponseEntity<MemberResponse> findByName(@PathVariable("deviceId") String deviceId) {
         Member findMember = memberRepository.findByDeviceId(deviceId).orElseThrow(() -> new IllegalArgumentException("아이디가 존재하지 않습니다"));
-        return new MemberResponse(findMember.getId(), findMember.getDeviceId());
+        return ResponseEntity.ok().body(new MemberResponse(findMember.getId(), findMember.getDeviceId()));
     }
 
     @PutMapping("/member/{deviceId}")
-    public MemberResponse saveNotExist(@PathVariable("deviceId") String deviceId) {
-        return memberService.saveNotExistMember(deviceId);
+    public ResponseEntity<MemberResponse> saveNotExist(@PathVariable("deviceId") String deviceId) {
+        return ResponseEntity.ok().body(memberService.saveNotExistMember(deviceId));
     }
 }
