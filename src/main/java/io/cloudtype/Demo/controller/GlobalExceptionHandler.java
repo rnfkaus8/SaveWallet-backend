52d4fc818c1e786ee938d6e755bbec8e9e75fe76
rnfkaus8 <kth4350@gmail.com>
2023-10-07 11:14:46 +0900

    add RestControllerAdvice

---
+++ src/main/java/io/cloudtype/Demo/controller/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package io.cloudtype.Demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
