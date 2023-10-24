0ac54b34a0154d4cb35f26c8086494cbe7c2e6a2
rnfkaus8 <kth4350@gmail.com>
2023-10-24 18:28:22 +0900

    add RuntimeExceptionHandler

--- src/main/java/io/cloudtype/Demo/exception/GlobalExceptionHandler.java
+++ src/main/java/io/cloudtype/Demo/exception/GlobalExceptionHandler.java
@@ -11,5 +11,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder().code("INVALID REQUEST").message(e.getMessage()).build());
     }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.builder().code("INTERNAL ERROR").message(e.getMessage()).build());
+    }
     
 }
