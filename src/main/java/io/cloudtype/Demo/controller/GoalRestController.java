dffad1ccf12cbe7c4dd063b7657af4544b46a388
rnfkaus8 <kth4350@gmail.com>
2023-10-27 10:10:41 +0900

    GoalResponse 를 controller에 적용

--- src/main/java/io/cloudtype/Demo/controller/GoalRestController.java
+++ src/main/java/io/cloudtype/Demo/controller/GoalRestController.java
@@ -3,6 +3,7 @@ package io.cloudtype.Demo.controller;
 import io.cloudtype.Demo.controller.request.FindGoalRequest;
 import io.cloudtype.Demo.controller.request.SaveGoalRequest;
 import io.cloudtype.Demo.controller.request.UpdateGoalRequest;
+import io.cloudtype.Demo.controller.response.GoalResponse;
 import io.cloudtype.Demo.domain.Goal;
 import io.cloudtype.Demo.domain.Member;
 import io.cloudtype.Demo.repository.GoalRepository;
@@ -30,20 +31,22 @@ public class GoalRestController {
     }
 
     @PutMapping("/goal")
-    public ResponseEntity<Goal> saveNotExistGoal(@RequestBody SaveGoalRequest request) {
+    public ResponseEntity<GoalResponse> saveNotExistGoal(@RequestBody SaveGoalRequest request) {
         Optional<Goal> optionalGoal = goalRepository.findTargetMonthGoal(request.getTargetMonth(), request.getMemberId());
         if (optionalGoal.isPresent()) {
-            return ResponseEntity.ok().body(optionalGoal.get());
+            Goal goal = optionalGoal.get();
+            return ResponseEntity.ok().body(new GoalResponse(goal.getId(), goal.getTargetMonth(), goal.getGoalPrice()));
         }
 
         Member member = memberRepository.findById(request.getMemberId()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
         Goal goal = Goal.builder().goalPrice(request.getGoalPrice()).member(member).targetMonth(request.getTargetMonth()).build();
-        return ResponseEntity.ok().body(goalRepository.save(goal));
+        Goal savedGoal = goalRepository.save(goal);
+        return ResponseEntity.ok().body(new GoalResponse(savedGoal.getId(), savedGoal.getTargetMonth(), savedGoal.getGoalPrice()));
     }
 
     @GetMapping("/goal")
-    public ResponseEntity<Goal> findByTargetMonth(FindGoalRequest request) {
+    public ResponseEntity<GoalResponse> findByTargetMonth(FindGoalRequest request) {
         Goal goal = goalRepository.findTargetMonthGoal(request.getTargetMonth(), request.getMemberId()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 목표입니다"));
-        return ResponseEntity.ok().body(goal);
+        return ResponseEntity.ok().body(new GoalResponse(goal.getId(), goal.getTargetMonth(), goal.getGoalPrice()));
     }
 }
